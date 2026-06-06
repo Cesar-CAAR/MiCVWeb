ea80418caf1380b9a45b6fabc958e5858d8bd729
Cesar-CAAR <cesar.andres5242@gmail.com>
2026-06-05 20:06:10 -0700

    Subiendo proyecto antes de ofuscarlo

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ window.APP_CONFIG = {
     email:     "cesar.andres5242@gmail.com",
     github:    "https://github.com/Cesar-CAAR",
 
-    version:   "2.0.0",
+    version:   "2.0.1",
 
     universidad: "Universidad Tecnológica del Sur de Sonora",
     carrera:     "Ingeniería en Desarrollo y Gestión de Software",
